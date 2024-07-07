6c98d3c5e66c688ff407e1b733697f39e8f35b26
alamin6688 <alamin.kzs06@gmail.com>
2024-07-08 00:01:52 +0600

    Banner section added

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,250 @@
+const Banner = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto mt-12 mb-12">
+      <div className="carousel w-full h-[400px] md:h-[700px]">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img
+            src="https://i.postimg.cc/YC2wzWk4/umbrella-chair-around-swimming-pool.jpg"
+            className="w-full  object-cover"
+          />
+          <div className="absolute flex items-center md:w-full h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+            <div className="text-white pl-8 md:pl-16 space-y-7 w-full md:w-1/2">
+              <h2 className="text-4xl md:text-6xl font-bold">
+                Discover the World with Nexus Travel
+              </h2>
+              <p>
+                Nexus Travel offers affordable & curated tours that promise
+                unforgettable adventures. Start your next adventure with Nexus
+                Travel today!
+              </p>
+              <div>
+                <button className="btn bg-[#FF3811] hover:bg-[#ff2a00] text-white mr-5 border-none">
+                  Explore More
+                </button>
+                <button className="btn btn-outline text-white hover:text-[#ff2a00] mb-4">
+                  Latest News
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a
+              href="#slide4"
+              className="btn btn-circle mr-5 bg-opacity-65 border-none"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide2"
+              className="btn btn-circle bg-[#FF3811] hover:bg-[#ff2a00] border-none text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="carousel-item relative w-full">
+          <img
+            src="https://i.postimg.cc/sxpRwPvW/villa-nature-water-travel-background.jpg"
+            className="w-full  object-cover"
+          />
+          <div className="absolute flex  items-center md:w-full h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+            <div className="text-white pl-8 md:pl-16 space-y-7 w-full md:w-1/2">
+              <h2 className="text-4xl md:text-6xl font-bold">
+                Discover the World with Nexus Travel
+              </h2>
+              <p>
+                Nexus Travel offers affordable & curated tours that promise
+                unforgettable adventures. Start your next adventure with Nexus
+                Travel today!
+              </p>
+              <div>
+                <button className="btn bg-[#FF3811] hover:bg-[#ff2a00] text-white mr-5 border-none">
+                  Explore More
+                </button>
+                <button className="btn btn-outline text-white hover:text-[#ff2a00] mb-4">
+                  Latest News
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a
+              href="#slide1"
+              className="btn btn-circle mr-5 bg-opacity-65 border-none"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide3"
+              className="btn btn-circle bg-[#FF3811] hover:bg-[#ff2a00] border-none text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide3" className="carousel-item relative w-full">
+          <img
+            src="https://i.postimg.cc/YC2wzWk4/umbrella-chair-around-swimming-pool.jpg"
+            className="w-full  object-cover"
+          />
+          <div className="absolute flex  items-center md:w-full h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+            <div className="text-white pl-8 md:pl-16 space-y-7 w-full md:w-1/2">
+              <h2 className="text-4xl md:text-6xl font-bold">
+                Discover the World with Nexus Travel
+              </h2>
+              <p>
+                Nexus Travel offers affordable & curated tours that promise
+                unforgettable adventures. Start your next adventure with Nexus
+                Travel today!
+              </p>
+              <div>
+                <button className="btn bg-[#FF3811] hover:bg-[#ff2a00] text-white mr-5 border-none">
+                  Explore More
+                </button>
+                <button className="btn btn-outline text-white hover:text-[#ff2a00] mb-4">
+                  Latest News
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a
+              href="#slide2"
+              className="btn btn-circle mr-5 bg-opacity-65 border-none"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide4"
+              className="btn btn-circle bg-[#FF3811] hover:bg-[#ff2a00] border-none text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide4" className="carousel-item relative w-full">
+          <img
+            src="https://i.postimg.cc/sxpRwPvW/villa-nature-water-travel-background.jpg"
+            className="w-full  object-cover"
+          />
+          <div className="absolute flex  items-center md:w-full h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+            <div className="text-white pl-8 md:pl-16 space-y-7 w-full md:w-1/2">
+              <h2 className="text-4xl md:text-6xl font-bold">
+                Discover the World with Nexus Travel
+              </h2>
+              <p>
+                Nexus Travel offers affordable & curated tours that promise
+                unforgettable adventures. Start your next adventure with Nexus
+                Travel today!
+              </p>
+              <div>
+                <button className="btn bg-[#FF3811] hover:bg-[#ff2a00] text-white mr-5 border-none">
+                  Explore More
+                </button>
+                <button className="btn btn-outline text-white hover:text-[#ff2a00] mb-4">
+                  Latest News
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a
+              href="#slide3"
+              className="btn btn-circle mr-5 bg-opacity-65 border-none"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide5"
+              className="btn btn-circle bg-[#FF3811] hover:bg-[#ff2a00] border-none text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide5" className="carousel-item relative w-full">
+          <img
+            src="https://i.postimg.cc/YC2wzWk4/umbrella-chair-around-swimming-pool.jpg"
+            className="w-full  object-cover"
+          />
+          <div className="absolute flex  items-center md:w-full h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+            <div className="text-white pl-8 md:pl-16 space-y-7 w-full md:w-1/2">
+              <h2 className="text-4xl md:text-6xl font-bold">
+                Discover the World with Nexus Travel
+              </h2>
+              <p>
+                Nexus Travel offers affordable & curated tours that promise
+                unforgettable adventures. Start your next adventure with Nexus
+                Travel today!
+              </p>
+              <div>
+                <button className="btn bg-[#FF3811] hover:bg-[#ff2a00] text-white mr-5 border-none">
+                  Explore More
+                </button>
+                <button className="btn btn-outline text-white hover:text-[#ff2a00] mb-4">
+                  Latest News
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a
+              href="#slide4"
+              className="btn btn-circle mr-5 bg-opacity-65 border-none"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide6"
+              className="btn btn-circle bg-[#FF3811] hover:bg-[#ff2a00] border-none text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+        <div id="slide6" className="carousel-item relative w-full">
+          <img
+            src="https://i.postimg.cc/sxpRwPvW/villa-nature-water-travel-background.jpg"
+            className="w-full  object-cover"
+          />
+          <div className="absolute flex  items-center md:w-full h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
+            <div className="text-white pl-8 md:pl-16 space-y-7 w-full md:w-1/2">
+              <h2 className="text-4xl md:text-6xl font-bold">
+                Discover the World with Nexus Travel
+              </h2>
+              <p>
+                Nexus Travel offers affordable & curated tours that promise
+                unforgettable adventures. Start your next adventure with Nexus
+                Travel today!
+              </p>
+              <div>
+                <button className="btn bg-[#FF3811] hover:bg-[#ff2a00] text-white mr-5 border-none">
+                  Explore More
+                </button>
+                <button className="btn btn-outline text-white hover:text-[#ff2a00] mb-4">
+                  Latest News
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+            <a
+              href="#slide5"
+              className="btn btn-circle mr-5 bg-opacity-65 border-none"
+            >
+              ❮
+            </a>
+            <a
+              href="#slide1"
+              className="btn btn-circle bg-[#FF3811] hover:bg-[#ff2a00] border-none text-white"
+            >
+              ❯
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="min-h-[calc(100vh-366px)]">
+            <Banner></Banner>
         </div>
     );
 };
