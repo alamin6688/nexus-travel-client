ac4f188aa50344f614d02be74b2afa738781139b
alamin6688 <alamin.kzs06@gmail.com>
2024-07-10 02:02:15 +0600

    Loader added on All Tourists Spots Page

--- src/Pages/AllTouristsSpot/AllTouristsSpot.jsx
+++ src/Pages/AllTouristsSpot/AllTouristsSpot.jsx
@@ -4,6 +4,8 @@ import AllTouristsSpotCard from "./AllTouristsSpotCard";
 
 const AllTouristsSpot = () => {
   const [touristSpot, setTouristSpot] = useState([]);
+  const [loading, setLoading] = useState(true); 
+
 
   useEffect(() => {
     axios
@@ -11,26 +13,33 @@ const AllTouristsSpot = () => {
       .then((response) => {
         console.log(response.data);
         setTouristSpot(response.data);
+        setLoading(false); 
       })
       .catch((error) => {
-        console.error("There was an error fetching the surveys!", error);
+        console.error("There was an error fetching the tourist spots!", error);
+        setLoading(false); 
       });
   }, []);
 
   return (
     <div className="bg-base-200">
       <div className="min-h-[calc(100vh-366px)] max-w-screen-xl mx-auto flex flex-col justify-center px-6 md:px-4 lg:px-2 pb-10">
-        <div className="text-center text-3xl md:text-4xl font-extrabold pt-8">
-          <h1>All Tourists Spots</h1>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
-          {touristSpot.map((card) => (
-            <AllTouristsSpotCard
-              key={card._id}
-              card={card}
-            ></AllTouristsSpotCard>
-          ))}
-        </div>
+        {loading ? (
+          <div className="flex items-center justify-center h-full">
+            <span className="loading loading-bars loading-lg"></span>
+          </div>
+        ) : (
+          <div>
+            <div className="text-center text-3xl md:text-4xl font-extrabold pt-8">
+              <h1>All Tourists Spots</h1>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
+              {touristSpot.map((card) => (
+                <AllTouristsSpotCard key={card._id} card={card} />
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
