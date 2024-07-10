0298dd4af7606d221267544c3446d4d5c6e02c7e
alamin6688 <alamin.kzs06@gmail.com>
2024-07-11 01:00:10 +0600

    Popular Tourist Spots sorting system added

--- src/Pages/Home/TouristsSpotsSection/TouristsSpotsSection.jsx
+++ src/Pages/Home/TouristsSpotsSection/TouristsSpotsSection.jsx
@@ -4,19 +4,31 @@ import AllTouristsSpotCard from "../../AllTouristsSpot/AllTouristsSpotCard";
 import { Link } from "react-router-dom";
 
 const TouristsSpotsSection = () => {
-  const [touristSpot, setTouristSpot] = useState([]);
+  const [touristSpots, setTouristSpots] = useState([]);
+  const [filteredSpots, setFilteredSpots] = useState([]);
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/allTouristSpot")
       .then((response) => {
         console.log(response.data);
-        setTouristSpot(response.data);
+        setTouristSpots(response.data);
+        // Initially show all spots
+        setFilteredSpots(response.data);
       })
       .catch((error) => {
         console.error("There was an error fetching the tourist spots!", error);
       });
   }, []);
+
+  // Function to sort spots by totalVisitorsPerYear in descending order
+  useEffect(() => {
+    setFilteredSpots(
+      [...touristSpots].sort(
+        (a, b) => b.totalVisitorsPerYear - a.totalVisitorsPerYear
+      )
+    );
+  }, [touristSpots]);
 
   return (
     <div className="mb-16">
@@ -25,14 +37,12 @@ const TouristsSpotsSection = () => {
           <h1>Popular Tourist Spots</h1>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-10">
-          {
-            touristSpot.slice(0, 6).map((card) => (
-              <AllTouristsSpotCard
-                key={card._id}
-                card={card}
-              ></AllTouristsSpotCard>
-            ))
-          }
+          {filteredSpots.slice(0, 6).map((card) => (
+            <AllTouristsSpotCard
+              key={card._id}
+              card={card}
+            ></AllTouristsSpotCard>
+          ))}
         </div>
         <div className="text-center">
           <Link to="/allTouristsSpot">
